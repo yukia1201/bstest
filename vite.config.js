fd67c0e92f555b099c3b78252b507b76be199ff7
yukia1201 <1112211014@gap.wzu.edu.tw>
2024-12-23 15:48:21 +0800

    1223

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+export default {
+
+  root: path.resolve(__dirname, 'src'),
+
+  build: {
+
+    outDir: '../dist'
+
+  },
+
+  server: {
+
+    port: 8080
+
+  }
+
+}
